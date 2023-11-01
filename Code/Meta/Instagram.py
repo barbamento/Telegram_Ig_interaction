c073da0d0d286fb1c243a50f5fdc666226c7e4c8
barbamento <lorenzo.alvisi@studio.unibo.it>
2023-11-01 10:44:25 +0100

    bugfix: cokkies expiration

--- Code/Meta/Instagram.py
+++ Code/Meta/Instagram.py
@@ -5,21 +5,33 @@ from logging import Logger
 class Instagram:
     def __init__(self, data_json: dict, logger: Logger) -> None:
         self.logger = logger
-        username = data_json["meta"]["instagram"]["username"]
-        pw = data_json["meta"]["instagram"]["password"]
+        self.username = data_json["meta"]["instagram"]["username"]
+        self.pw = data_json["meta"]["instagram"]["password"]
         self.IG_App = Client()
-        self.IG_App.login(username=username, password=pw)
-        self.logger.info(f"{username} logged")
+        self.IG_App.login(username=self.username, password=self.pw)
+        self.logger.info(f"{self.username} logged")
 
     def post_image(
         self,
         caption: str,
         image_path: list[str],
     ):
-        if len(image_path) == 1:
-            res = self.IG_App.photo_upload(image_path[0], caption)
-            self.logger.info("photo successfully posted")
-        else:
-            res = self.IG_App.album_upload(image_path, caption)
-            self.logger.info("photo successfully posted")
-        return res
+        try:
+            if len(image_path) == 1:
+                res = self.IG_App.photo_upload(image_path[0], caption)
+                self.logger.info("photo successfully posted")
+            else:
+                res = self.IG_App.album_upload(image_path, caption)
+                self.logger.info("photo successfully posted")
+            return res
+        except Exception as e:
+            self.logger.warning("Could not post  photo. Relogging")
+            self.IG_App = Client()
+            self.IG_App.login(username=self.username, password=self.pw)
+            if len(image_path) == 1:
+                res = self.IG_App.photo_upload(image_path[0], caption)
+                self.logger.info("photo successfully posted")
+            else:
+                res = self.IG_App.album_upload(image_path, caption)
+                self.logger.info("photo successfully posted")
+            return res
